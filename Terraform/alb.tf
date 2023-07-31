33171ffc710cd27cb0340b50bd12e484a6c0943e
Jennifer <dubrajennifer@gmail.com>
2023-07-31 18:38:10 +0200

    renombrado de blue-green target groups

--- Terraform/alb.tf
+++ Terraform/alb.tf
@@ -1,11 +1,11 @@
-resource "aws_lb" "bluegreen_alb" {
+resource "aws_lb" "ab_alb" {
   name               = "BlueGreenALB"
   load_balancer_type = "application"
   subnets            = [aws_subnet.subnet-a.id, aws_subnet.subnet-b.id]
 }
 
-resource "aws_lb_target_group" "blue_target" {
-  name     = "BlueTarget"
+resource "aws_lb_target_group" "a_target" {
+  name     = "ATarget"
   port     = 5080
   protocol = "HTTP"
   vpc_id   = aws_vpc.projen-env.id
@@ -18,8 +18,8 @@ resource "aws_lb_target_group" "blue_target" {
   }
 }
 
-resource "aws_lb_target_group" "green_target" {
-  name     = "GreenTarget"
+resource "aws_lb_target_group" "b_target" {
+  name     = "BTarget"
   port     = 5080
   protocol = "HTTP"
   vpc_id   = aws_vpc.projen-env.id
@@ -32,21 +32,22 @@ resource "aws_lb_target_group" "green_target" {
   }
 }
 
-resource "aws_lb_listener" "bluegreen_alb" {
-  load_balancer_arn = aws_lb.bluegreen_alb.arn
+resource "aws_lb_listener" "ab_alb" {
+  load_balancer_arn = aws_lb.ab_alb.arn
   port              = 5080
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.blue_target.arn
+    target_group_arn = aws_lb_target_group.a_target.arn
   }
 
 }
 
-resource "aws_lb_target_group_attachment" "blue_attachment" {
-  target_group_arn = aws_lb_target_group.blue_target.arn
+resource "aws_lb_target_group_attachment" "a_attachment" {
+  target_group_arn = aws_lb_target_group.a_target.arn
   target_id        = aws_instance.app_openmeetings_server_1.id
   port             = 5080
 }
+
 
